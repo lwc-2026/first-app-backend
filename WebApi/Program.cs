38668e0ecf76c3eb29b39292dfee94abe2cc6535
ericsong95 <songkaiwa05@gmail.com>
2026-08-31 01:42:05 +0800

    last update

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,5 +1,4 @@
 using DataAccess.Dbcontexts;
-using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,20 +6,20 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-builder.Services.AddDbContext<AppDbContext>(options =>
-{
-    options.UseSqlite(
-        builder.Configuration.GetConnectionString("DefaultConnection")
-    );
-});
+// builder.Services.AddOpenApi();
+// builder.Services.AddDbContext<AppDbContext>(options =>
+// {
+//     options.UseSqlite(
+//         builder.Configuration.GetConnectionString("DefaultConnection")
+//     );
+// });
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    // app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
