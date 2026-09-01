d8528e3b369547b09f650544fa464d0cc96f4706
Eric Song <songkaiwa05@gmail.com>
2026-09-01 15:54:14 +0800

    add RESTful UsersController (In progress

---
+++ WebApi/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using DataAccess.Dbcontexts;
+using DataAccess.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class UsersController(AppDbContext context) : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IReadOnlyCollection<AppUser>> GetUsers()
+        {
+            var users = context.Users.ToList();
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<AppUser>? GetUser(string id)
+        {
+            var user = context.Users.Find(id);
+            if (user == null) return NotFound();
+            return user;
+        }
+    }
+}
