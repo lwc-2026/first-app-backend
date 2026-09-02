9c22c7d36e8dfad7758bf450266a91fd57bed249
Eric Song <songkaiwa05@gmail.com>
2026-09-02 14:55:21 +0800

    add restful controller

--- WebApi/Controllers/UserController.cs
+++ WebApi/Controllers/UserController.cs
@@ -1,6 +1,7 @@
 ﻿using DataAccess.Dbcontexts;
 using DataAccess.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebApi.Controllers
 {
@@ -21,6 +22,46 @@ namespace WebApi.Controllers
             var user = context.Users.Find(id);
             if (user == null) return NotFound();
             return user;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<AppUser>?> CreateUser(AppUser user)
+        {
+            context.Users.Add(user);
+            await context.SaveChangesAsync();
+            return CreatedAtAction("GetUser", new { id = user.Id }, user);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUser(string id, AppUser user)
+        {
+            if (id != user.Id)
+            {
+                return BadRequest();
+            }
+
+            context.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                await context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUser(string id)
+        {
+            AppUser? user = await context.Users.FindAsync(id);
+            if(user == null) return NotFound();
+            context.Users.Remove(user);
+            await context.SaveChangesAsync();
+            return NoContent();
         }
     }
 }
