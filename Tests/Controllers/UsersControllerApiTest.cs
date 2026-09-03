886445bd247831d8456e3fb11aa4bf8246fc4f16
Eric Song <songkaiwa05@gmail.com>
2026-09-03 14:04:46 +0800

    remove unnecessary code

--- Tests/Controllers/UsersControllerApiTest.cs
+++ Tests/Controllers/UsersControllerApiTest.cs
@@ -1,28 +1,25 @@
 using DataAccess.Dbcontexts;
-using DataAccess.Entities;
 using Tests.Factories;
 using System.Net;
-using System.Text.Json;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Tests.Controllers;
 
 public class UsersControllerApiTest: IClassFixture<TestDbFactory>
 {
-    private TestDbFactory factory;
+    private TestDbFactory testDbFactory;
     private HttpClient client;
 
-    public UsersControllerApiTest(TestDbFactory _factory)
+    public UsersControllerApiTest(TestDbFactory factory)
     {
-        factory = _factory;
-        client = factory.CreateClient();
+        testDbFactory = factory;
+        client = testDbFactory.CreateClient();
     }
 
     [Fact]
     public async Task TestDeleteUser()
     {
-        using var scope = factory.Services.CreateScope();
+        using var scope = testDbFactory.Services.CreateScope();
         var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
         var user = AppUserFactory.Create();
         context.Users.Add(user);
@@ -30,19 +27,10 @@ public class UsersControllerApiTest: IClassFixture<TestDbFactory>
         Assert.Single(context.Users);
 
         var response = await client.DeleteAsync($"/api/users/{user.Id}");
-
-        Console.WriteLine("response.StatusCode {0}", response.StatusCode);
-
         Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
-
-        // using var verifyScope = factory.Services.CreateAsyncScope();
-        // var verifyDb = verifyScope.ServiceProvider.GetRequiredService<AppDbContext>();
-        // var deleted = await verifyDb.Users.FindAsync(user.Id);
 
         context.ChangeTracker.Clear();
         var deleted = await context.Users.FindAsync(user.Id);
-        Console.WriteLine("Users count {0}", await context.Users.CountAsync());
-
         Assert.Null(deleted);
     }
 
