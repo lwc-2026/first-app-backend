6bc3e3fa64f97167a4ed32caa3ceacc65fa3b20e
Eric Song <songkaiwa05@gmail.com>
2026-09-04 12:12:50 +0800

    update api routes

---
+++ WebApi/Controllers/HealthCheckController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("/api/healthcheck")]
+    public class HealthCheckController : Controller
+    {
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> HealthCheck()
+        {
+            return await Task.FromResult(Ok("Healthy"));
+        }
+    }
+}
