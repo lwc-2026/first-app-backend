b6866053529a3be63279ce961ca7b9939559b219
Eric Song <songkaiwa05@gmail.com>
2026-09-04 12:12:20 +0800

    update migration files

---
+++ DataAccess/Migrations/20260903103528_InitCreate.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ DataAccess/Migrations/20260904033810_AddHealthcheckProcedure.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddHealthcheckProcedure : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""
+            CREATE OR ALTER PROCEDURE HealthCheck
+            -- No parameters for HealthCheck
+            AS
+            BEGIN
+            SET NOCOUNT ON;
+            SELECT
+                GETUTCDATE() AS UtcTime,
+                CAST(
+                    GETUTCDATE()
+                    AT TIME ZONE 'UTC'
+                    AT TIME ZONE 'China Standard Time'
+                    AS DATETIME2
+                ) AS MacauTime;
+            END
+            """);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""
+            DROP PROCEDURE IF EXISTS HealthCheck;
+            """);
+        }
+    }
+}
