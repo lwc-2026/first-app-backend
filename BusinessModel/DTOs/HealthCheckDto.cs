ce141b173e3d64d75cc20d9235176ed4a5526f7b
Eric Song <songkaiwa05@gmail.com>
2026-09-07 11:48:54 +0800

    add project

---
+++ BusinessModel/DTOs/HealthCheckDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BusinessModel.Enums;
+
+namespace BusinessModel.DTOs
+{ 
+    public class HealthCheckDto
+    {
+        public DateTime? UtcTime { get; set; }
+        public DateTime? MacauTime { get; set; }
+        public HealthCheckStatus HealthCheckStatus { get; set; } = HealthCheckStatus.Unhealthy;
+
+        public void UpdateHealthCheckStatusToHealthy()
+        {
+            HealthCheckStatus = HealthCheckStatus.Healthy;
+        }
+
+        public bool IsHealthy()
+        {
+            return HealthCheckStatus == HealthCheckStatus.Healthy;
+        }
+    }
+}
---
+++ BusinessModel/Enums/HealthCheckEnums.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessModel.Enums
+{
+    public enum HealthCheckStatus: int
+    {
+        Healthy = 1,
+        Unhealthy = 2
+    }
+}
