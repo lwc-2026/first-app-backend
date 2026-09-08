06a4b2a3da7a24cb85af20b07924fdf31374c0c8
Eric Song <songkaiwa05@gmail.com>
2026-09-08 18:39:48 +0800

    add http request

---
+++ WebApi/Requests/CreateUserHttpRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApi.Requests
+{
+    public class CreateUserServiceRequest
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
