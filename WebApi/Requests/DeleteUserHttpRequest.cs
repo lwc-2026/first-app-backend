78d21a5b7a506a1431e93a932bdffa41dafdc5cc
Eric Song <songkaiwa05@gmail.com>
2026-09-09 14:43:30 +0800

    add http requests for users controller

---
+++ WebApi/Requests/DeleteUserHttpRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace WebApi.Requests
+{
+    public class DeleteUserHttpRequest
+    {
+        public required string Id { get; set; } 
+    }
+}
--- WebApi/Requests/UpdateUserHttpRequest.cs
+++ WebApi/Requests/UpdateUserHttpRequest.cs
@@ -2,16 +2,14 @@
 
 namespace WebApi.Requests
 {
-    public class CreateUserHttpRequest
+    public class UpdateUserHttpRequest
     {
-        [Required]
         [StringLength(100, ErrorMessage = "Username cannot be longer than 100 characters")]
-        public string Username { get; set; } = string.Empty;
-        [Required]
-        [EmailAddress(ErrorMessage = "Invalid email address")]
-        public string Email { get; set; } = string.Empty;
+        public string? Username { get; set; }
         [Required]
         [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$", ErrorMessage = "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, and one number.")]
-        public string Password { get; set; } = string.Empty;
+        public string? Password { get; set; }
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string? Email { get; set; }
     }
 }
