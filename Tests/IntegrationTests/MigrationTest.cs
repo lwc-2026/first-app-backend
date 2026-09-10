9840ff79d7cf7f9f2ea3f9d50a5b201f21a4b20b
Eric Song <songkaiwa05@gmail.com>
2026-09-10 16:02:07 +0800

    update migration tests

--- Tests/IntegrationTests/MigrationTest.cs
+++ Tests/IntegrationTests/MigrationTest.cs
@@ -1,8 +1,10 @@
+using BusinessModel.DTOs;
 using DataAccess.Dbcontexts;
 using FluentAssertions;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Tests.Fixtures;
+using BusinessModel.Enums;
 
 namespace Tests.IntegrationTests;
 
@@ -31,9 +33,31 @@ public class MigrationTest : IClassFixture<DatabaseFixture>
         var migrations = await context.Database.GetAppliedMigrationsAsync();
 
         migrations.Should().NotBeEmpty();
+        migrations.Should().Contain("20260907030308_InitialCreate");
+        migrations.Should().Contain("20260907030557_AddHealthCheckStoredProcedure");
 
         var pending = await context.Database.GetPendingMigrationsAsync();
 
         pending.Should().BeEmpty();
+
+        var userCount = await context.Users.CountAsync();
+
+        userCount.Should().Be(0);
+    }
+
+    [Fact]
+    public async Task Test_HealthCheck_Stored_Procedure_can_be_executed()
+    {
+        using var scope = _fixture.Services.CreateScope();
+
+        var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+        var result = (await context.Database.SqlQuery<HealthCheckDto>($"EXEC dbo.HEALTHCHECK;")
+                .ToListAsync())
+                .AsEnumerable().FirstOrDefault();
+
+        result.Should().NotBeNull();
+
+        result!.HealthCheckStatus.Should().Be(HealthCheckStatus.Healthy);
     }
 }
