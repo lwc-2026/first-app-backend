75e101f27b4a4b5a5d1028dad81ccf960bf67af8
Eric Song <songkaiwa05@gmail.com>
2026-09-11 11:30:05 +0800

    add a collection class for integration test

---
+++ Tests/Collections/IntegrationCollection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tests.Fixtures;
+
+namespace Tests.Collections
+{
+    [CollectionDefinition("Integration")]
+    public class IntegrationCollection: ICollectionFixture<DatabaseFixture>
+    {
+    }
+}
