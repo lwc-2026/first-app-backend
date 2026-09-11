1780b1056b42ea20a43ecd26d36543e828e6b89a
Eric Song <songkaiwa05@gmail.com>
2026-09-11 11:34:47 +0800

    move HealthCheckServvice unit test to integration test

---
+++ Tests/IntegrationTests/HealthCheckServiceTest.cs
@@ -0,0 +1,33 @@
+﻿using BusinessModel.DTOs;
+using BusinessModel.Enums;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Tests.Fixtures;
+
+namespace Tests.IntegrationTests
+{
+    [Collection("Integration")]
+    public class HealthCheckServiceTest(DatabaseFixture fixture) : IntegrationTestBase(fixture)
+    {
+        [Fact]
+        public async Task Can_return_health_check_dto()
+        {
+            using var scope = _fixture.CreateScope();
+
+            var healthCheckService = scope.ServiceProvider.GetRequiredService<IHealthCheckService>();
+
+            var result = await healthCheckService.HealthCheck();
+
+            result.Should().NotBeNull();
+            result.Should().BeEquivalentTo(
+                new HealthCheckDto
+                {
+                    HealthCheckStatus = HealthCheckStatus.Healthy
+                });        
+        }
+    }
+}
