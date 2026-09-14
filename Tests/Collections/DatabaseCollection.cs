85dbca6d69ca42571dd58e83c0f1565695d8a3e0
Eric Song <songkaiwa05@gmail.com>
2026-09-14 18:50:17 +0800

    update test collection

--- Tests/Collections/DatabaseCollection.cs
+++ Tests/Collections/DatabaseCollection.cs
@@ -1,9 +1,24 @@
-using Xunit;
 using Tests.Fixtures;
 
 namespace Tests.Collections;
 
 [CollectionDefinition("Database")]
-public class DatabaseCollection: ICollectionFixture<DatabaseFixture>
+public class DatabaseCollection: ICollectionFixture<DatabaseFixture>, IAsyncLifetime
 {
+    protected DatabaseFixture _fixture;
+
+    public DatabaseCollection(DatabaseFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    public async Task InitializeAsync()
+    {
+        await _fixture.ResetDatabaseAsync();
+    }
+
+    public async Task DisposeAsync()
+    {
+        await _fixture.DisposeAsync();
+    }
 }
--- Tests/Collections/IntegrationCollection.cs
+++ Tests/Collections/IntegrationCollection.cs
@@ -1,14 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Tests.Factories;
 using Tests.Fixtures;
 
 namespace Tests.Collections
 {
     [CollectionDefinition("Integration")]
-    public class IntegrationCollection: ICollectionFixture<DatabaseFixture>, IAsyncLifetime
+    public class IntegrationCollection: ICollectionFixture<DatabaseFixture>, IAsyncLifetime, ICollectionFixture<UserFactory>
+
     {
-        protected DatabaseFixture _fixture = new DatabaseFixture();
+        protected DatabaseFixture _fixture;
+        protected UserFactory _userFactory;
+
+        public IntegrationCollection(DatabaseFixture fixture, UserFactory userFactory)
+        {
+            _fixture = fixture;
+            _userFactory = userFactory;
+        }
+
         public async Task InitializeAsync()
         {
             await _fixture.ResetDatabaseAsync();
