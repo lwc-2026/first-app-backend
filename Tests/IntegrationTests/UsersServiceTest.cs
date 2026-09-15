9778f4af47ef1da2688a8e015f1139ae189cbb3e
Eric Song <songkaiwa05@gmail.com>
2026-09-15 18:17:28 +0800

    add test for UsersService.Update and UsersService.DeleteUser

--- Tests/IntegrationTests/UsersServiceTest.cs
+++ Tests/IntegrationTests/UsersServiceTest.cs
@@ -1,13 +1,16 @@
-﻿using DataAccess.Dbcontexts;
+﻿using Bogus;
+using DataAccess.Dbcontexts;
 using DataAccess.Entities;
 using FluentAssertions;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Service.Implementations;
+using Service.Requests;
 using System;
 using System.Collections.Generic;
 using System.Data.Common;
 using System.Text;
+using System.Text.Json;
 using Tests.Factories;
 using Tests.Fixtures;
 
@@ -18,20 +21,23 @@ public class UsersServiceTest
 {
     private readonly DatabaseFixture _fixture;
     private readonly UserFactory _userFactory;
+    private readonly Faker _faker;
 
     public UsersServiceTest(DatabaseFixture fixture, UserFactory userFactory)
     {
         _fixture = fixture;
         _userFactory = userFactory;
-        Console.WriteLine($"UserServiceTest Initialized: {GetHashCode()}");
+        _faker = new Faker();
     }
+
+    internal UsersService SetupUserService(AppDbContext context) => new UsersService(context);
 
     [Fact]
     public async Task Can_get_user_lists()
     {
         using var scope = _fixture.CreateScope();
         var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-
+        var userService = this.SetupUserService(context);
         List<AppUser> appUsers = _userFactory.CreateMany(10);
 
         foreach (AppUser user in appUsers)
@@ -45,14 +51,10 @@ public class UsersServiceTest
 
         context.ChangeTracker.Clear();
 
-        UsersService userService = new UsersService(context);
-
         var result = await userService.GetUsersList();
 
         // assertions
-
         result.Should().BeOfType<List<AppUser>>();
-        // result.Should().HaveCount(appUsers.Count);
     }
 
     [Fact]
@@ -60,7 +62,7 @@ public class UsersServiceTest
     {
         using var scope = _fixture.CreateScope();
         var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-
+        var userService = this.SetupUserService(context);
         AppUser appUser = _userFactory.Create();
 
         context.Users.Add(appUser);
@@ -68,8 +70,6 @@ public class UsersServiceTest
         await context.SaveChangesAsync();
 
         context.ChangeTracker.Clear();
-
-        UsersService userService = new UsersService(context);
 
         AppUser? result = await userService.GetUserById(appUser.Id);
 
@@ -79,5 +79,117 @@ public class UsersServiceTest
         result.Should().BeEquivalentTo(appUser);
     }
 
+    [Fact]
+    public async Task Can_create_one_user()
+    {
+        using var scope = _fixture.CreateScope();
+        var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        var userService = this.SetupUserService(context);
+
+        string username = _faker.Internet.UserName();
+        string email = _faker.Internet.Email();
+        string password = _faker.Internet.Password();
+
+        CreateUserServiceRequest serviceRequest = new CreateUserServiceRequest(
+            username: username,
+            email: email,
+            password: password
+        );
+
+        AppUser? result = await userService.CreateUserAsync(serviceRequest);
+
+        context.ChangeTracker.Clear();
+
+        var query = context.Users.Where(x => x.Username == username && x.Email == email && x.Password == password);
+
+        List<AppUser> queryResult = await query.ToListAsync();
+
+        queryResult.Should().HaveCount(1);
+        queryResult.Single().Username.Should().Be(username);
+        queryResult.Single().Email.Should().Be(email);
+        queryResult.Single().Password.Should().Be(password);
+    }
+
+
+    [Fact]
+    public async Task Can_Delete_one_user()
+    {
+        using var scope = _fixture.CreateScope();
+        var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        var userService = this.SetupUserService(context);
+
+        AppUser user = _userFactory.Create();
+
+        context.Users.Add(user);
+        await context.SaveChangesAsync();
+
+        DeleteUserServiceRequest serviceRequest = new DeleteUserServiceRequest(user.Id);
+
+        context.ChangeTracker.Clear();
+
+        var query = context.Users.Where(x => x.Id == user.Id);
+
+        List<AppUser> queryResult = await query.ToListAsync();
+
+        queryResult.Should().HaveCount(1);
+        queryResult.Single().Id.Should().Be(user.Id);
+        queryResult.Single().Username.Should().Be(user.Username);
+        queryResult.Single().Email.Should().Be(user.Email);
+        queryResult.Single().Password.Should().Be(user.Password);
+
+        await userService.DeleteUserAsync(serviceRequest);
+
+        context.ChangeTracker.Clear();
+
+        queryResult = await query.ToListAsync();
+
+        queryResult.Should().BeNullOrEmpty();
+    }
+
+    [Fact]
+    public async Task Can_Update_one_user()
+    {
+        using var scope = _fixture.CreateScope();
+        var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        var userService = this.SetupUserService(context);
+
+        AppUser user = _userFactory.Create();
+        context.Users.Add(user);
+        await context.SaveChangesAsync();
+
+        var query = context.Users.Where(x => x.Id == user.Id);
+
+        context.ChangeTracker.Clear();
+
+        List<AppUser> queryResult = await query.ToListAsync();
+
+        queryResult.Should().HaveCount(1);
+        queryResult.Single().Id.Should().Be(user.Id);
+        queryResult.Single().Username.Should().Be(user.Username);
+        queryResult.Single().Email.Should().Be(user.Email);
+        queryResult.Single().Password.Should().Be(user.Password);
+
+        string updatedUsername = _faker.Internet.UserName();
+        string updatedEmail = _faker.Internet.Email();
+        string updatedPassword = _faker.Internet.Password();
+
+        UpdateUserServiceRequest serviceRequest = new UpdateUserServiceRequest() { 
+            Id = user.Id,
+            Username = updatedUsername,
+            Email = updatedEmail,
+            Password = updatedPassword
+        };
+
+        var response = await userService.UpdateUserAsync(serviceRequest);
+
+        context.ChangeTracker.Clear();
+        queryResult = await query.ToListAsync();
+
+        queryResult.Should().HaveCount(1);
+        queryResult.Single().Id.Should().Be(user.Id);
+        queryResult.Single().Username.Should().Be(updatedUsername);
+        queryResult.Single().Email.Should().Be(updatedEmail);
+        queryResult.Single().Password.Should().Be(updatedPassword);
+    }
 }
 
