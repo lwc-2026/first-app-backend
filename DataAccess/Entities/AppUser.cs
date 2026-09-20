5bd9f2262c8d0527e4e1dc6acb280c8f7a479463
ericsong95 <songkaiwa05@gmail.com>
2026-09-21 00:55:01 +0800

    add password hash to users table

--- DataAccess/Entities/AppUser.cs
+++ DataAccess/Entities/AppUser.cs
@@ -6,13 +6,12 @@ public class AppUser
 {
     [Key]
     public string Id { get; set; } = Guid.NewGuid().ToString();
-
     [Required]
     public required string Username { get; set; }
-
     [EmailAddress]
     public string? Email { get; set; }
-
     [Required]
-    public required string Password { get; set; }
+    public required byte[] PasswordHash { get; set; }
+    [Required]
+    public required byte[] PasswordSalt { get; set; }
 }
