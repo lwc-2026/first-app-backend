c78abfb0bd295a8f8af29e7afbc476a8092f2173
Eric Song <songkaiwa05@gmail.com>
2026-09-25 11:19:21 +0800

    add di

--- Tests/Fixtures/TestWebApplicationFactory.cs
+++ Tests/Fixtures/TestWebApplicationFactory.cs
@@ -2,6 +2,7 @@ using DataAccess.Dbcontexts;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Tests.Fixtures;
@@ -10,6 +11,7 @@ public class TestWebApplicationFactory : WebApplicationFactory<Program>
 {
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
+        builder.UseEnvironment("Testing");
         builder.ConfigureServices(services =>
         {
             // remove original registration
@@ -23,11 +25,14 @@ public class TestWebApplicationFactory : WebApplicationFactory<Program>
 
             // add testing registration
 
-            services.AddDbContext<AppDbContext>(options =>
+            services.AddScoped<SoftDeleteInterceptor>();
+            services.AddScoped<AuditTimestampInterceptor>();
+            services.AddDbContext<AppDbContext>((sp, options) =>
             {
                 options.UseSqlServer("Server=sql1-test,1433;Database=firstapp;User Id=sa;Password=P@ssw0rd123;Encrypt=True;TrustServerCertificate=True;");
+                options.AddInterceptors(sp.GetRequiredService<SoftDeleteInterceptor>());
+                options.AddInterceptors(sp.GetRequiredService<AuditTimestampInterceptor>());
             });
         });
-        builder.UseEnvironment("Testing");
     }
 }
