de3f261cb8f869280527265a2b715a3fe49292ac
Eric Song <songkaiwa05@gmail.com>
2026-09-25 18:13:35 +0800

    remove unused property id

--- Service/Requests/AssignAssetServiceRequest.cs
+++ Service/Requests/AssignAssetServiceRequest.cs
@@ -4,14 +4,12 @@ namespace Service.Requests;
 
 public class AssignAssetServiceRequest
 {
-    public int Id { get; }
     public string UserId { get; }
     public int AssetId { get; }
     public string Description { get; }
 
-    public AssignAssetServiceRequest(int id, string userId, int assetId, string description)
+    public AssignAssetServiceRequest(string userId, int assetId, string description)
     {
-        Id = id;
         UserId = userId;
         AssetId = assetId;
         Description = description;
--- Service/Requests/ReturnAssetServiceRequest.cs
+++ Service/Requests/ReturnAssetServiceRequest.cs
@@ -4,14 +4,12 @@ namespace Service.Requests;
 
 public class ReturnAssetServiceRequest
 {
-    public int Id { get; }
     public string UserId { get; }
     public int AssetId { get; }
     public string Description { get; }
 
-    public ReturnAssetServiceRequest(int id, string userId, int assetId, string description)
+    public ReturnAssetServiceRequest(string userId, int assetId, string description)
     {
-        Id = id;
         UserId = userId;
         AssetId = assetId;
         Description = description;
